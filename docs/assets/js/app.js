44cd724bfc03a445d2d06193dc321217acdf7811
segawa-tt <sgtt.xr.b@gmail.com>
2025-02-06 14:26:45 +0900

    ステージ追加

--- docs/assets/js/app.js
+++ docs/assets/js/app.js
@@ -9,7 +9,8 @@ function initializeGame() {
     const backgrounds = [
         { src: "./assets/images/2.png", x: 500, y: 650 },
         { src: "./assets/images/3.png", x: 1759, y: 395},
-        { src: "./assets/images/Renoir.jpg", x: 420, y: 140}
+        { src: "./assets/images/Renoir.jpg", x: 420, y: 140},
+        { src: "./assets/images/Napoleon.jpg", x: 100, y: 960}
     ];
 
     // 偽物画像リスト
